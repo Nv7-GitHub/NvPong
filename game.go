e1c7313e034f61fbebb312b383f650b9f2a03774
Nv7 <junk4Nv7@gmail.com>
2020-12-05 18:11:18 -0800

    Game controls

--- game.go
+++ game.go
@@ -5,19 +5,31 @@ import (
 	"github.com/go-gl/glfw/v3.2/glfw"
 )
 
-var x = 0
-var y = 0
+var x float32 = 0
+var y float32 = 0
 
 func draw(window *glfw.Window, program uint32) {
 	gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
 	gl.UseProgram(program)
 
 	gl.Begin(gl.QUADS)
-	gl.Vertex2f(0, 0)
-	gl.Vertex2f(0, 0.5)
-	gl.Vertex2f(0.5, 0.5)
-	gl.Vertex2f(0.5, 0)
+	gl.Vertex2f(x, y)
+	gl.Vertex2f(x, y+0.5)
+	gl.Vertex2f(x+0.5, y+0.5)
+	gl.Vertex2f(x+0.5, y)
 	gl.End()
+
+	if isPressed(window, glfw.KeyRight) {
+		x += 0.01
+	} else if isPressed(window, glfw.KeyLeft) {
+		x -= 0.01
+	}
+
+	if isPressed(window, glfw.KeyUp) {
+		y += 0.01
+	} else if isPressed(window, glfw.KeyDown) {
+		y -= 0.01
+	}
 
 	glfw.PollEvents()
 	window.SwapBuffers()
